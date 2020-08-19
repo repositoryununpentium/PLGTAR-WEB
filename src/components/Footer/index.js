b6a1e79fa54d4b74b9610e856282a19a4142a058
Ununpentium <ununpentium@outlook.com.br>
2020-08-19 07:24:26 -0300

    PLGTAR-371220200818

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as STYLE from './style';
+
+function Footer() {
+    return (
+        <STYLE.Container>
+            <span>Plataforma de Lançamento - Gerenciador de Tarefas</span>
+        </STYLE.Container>
+    )
+}
+
+export default Footer;
--- src/view/Principal/index.js
+++ src/view/Principal/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import * as STYLE from './style';
 
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Principal() {
     return (
         <STYLE.Container>
             <Header/>
+            <Footer/>
         </STYLE.Container>
     )
 }
